11f2024051eae158a3c53db85a1bc88b5e47f180
Sven Schroeder <schuen@gmx.de>
2025-02-24 08:38:36 +0100

    fix typo in UserRepo

--- src/main/java/com/svenschroeder/security_example/model/MyUserRepository.java
+++ src/main/java/com/svenschroeder/security_example/model/MyUserRepository.java
@@ -8,7 +8,7 @@ public interface MyUserRepository extends JpaRepository<MyUser, Long> {
     /*
      * This method has to be exactly named this way:
      * Spring calls this method query derivation.
-     * FindBy and then the name of the field is to be queried.
+     * FindBy and then the name of the field that is to be queried.
      */
     Optional<MyUser> findByUsername(String username);
 
